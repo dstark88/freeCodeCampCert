d0a7f84bc048f5a2086eaca21634b8bb3c8df0ea
Denise Stark <denise.stark@medtronic.com>
2020-03-11 15:33:17 -0600

    new lessons

---
+++ javaScript/7-boolean.js
@@ -0,0 +1,21 @@
+// function isLess(a, b) {
+//     // Only change code below this line
+//     if (a < b) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//     // Only change code above this line
+//   }
+  
+//   isLess(10, 15);
+
+function isLess(a, b) {
+    // Only change code below this line
+
+
+        console.log(a < b);
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
