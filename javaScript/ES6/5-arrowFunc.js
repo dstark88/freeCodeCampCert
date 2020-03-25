663780fba28c84778f030e624f88d078def10872
dstark88 <starkwalker88@gmail.com>
2020-03-24 21:43:46 -0600

    new lesson

---
+++ javaScript/ES6/5-arrowFunc.js
@@ -0,0 +1,23 @@
+// Use Arrow Functions to write concise anonymous functions
+    // Example
+    // const myFunc = function() {
+    //     const myVar = "value";
+    //     return myVar;
+    //   }
+
+    // ES6 Example
+    // const myFunc = () => {
+    //     const myVar = "value";
+    //     return myVar;
+    //   }
+
+    // Example without return word
+    // const myFunc = () => "value";
+
+    // Lesson
+    // var magic = function() {
+    //     "use strict";
+    //     return new Date();
+    //   };
+    const magic = () => new Date();
+      
